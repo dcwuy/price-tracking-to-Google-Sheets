d1c5b4d728827ba95f2e219f2c83311bfe2bdf03
Dương Chấn Huy <109254074+dcwuy@users.noreply.github.com>
2025-09-14 08:08:24 -0700

    Create bimatquocgia.py

---
+++ bimatquocgia.py
@@ -0,0 +1,17 @@
+# Nơi lưu giữ bí mật quốc gia - Thiết lập chương trình
+# Environment secrets
+
+# Khóa - API Keys
+SCRAPINGANT = "<SCRAPING ANT API>"
+OXYLABS_ACCOUNT = "<OXYLABS API ACCOUNT>"
+OXYLABS_PASSWORD = "<OXYLABS API PASSWORD>"
+
+# Timezone
+TIMEZONE = "US/Central"
+
+# Google Sheet
+SHEET_URL = "<Google Sheets URL>"
+SERVICE_ACCOUNT = "<account.json>"
+
+# Zip code for Amazon lookups
+ZIP_CODE =  "<ZIP CODE>"
