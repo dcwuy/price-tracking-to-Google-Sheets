5370ed58eb76686d72bdbb437799316c8a9c345f
Dương Chấn Huy <109254074+dcwuy@users.noreply.github.com>
2025-10-10 10:17:52 -0700

    More resilient
    Using more API for more IP ranges

--- main.py
+++ main.py
@@ -16,6 +16,9 @@ GOOGLE_SERVICE_ACCOUNT_FILE = bimatquocgia.SERVICE_ACCOUNT
 SCRAPINGANT_API_KEY = bimatquocgia.SCRAPINGANT
 OXYLABS_ACCOUNT = bimatquocgia.OXYLABS_ACCOUNT
 OXYLABS_PASSWORD = bimatquocgia.OXYLABS_PASSWORD
+HASDATA_API_KEY = bimatquocgia.HASDATA
+WebScrapingDotAi_API_KEY = bimatquocgia.WebScrapingDotAi
+SCRAPINGROBOT_API_KEY = bimatquocgia.SCRAPINGROBOT
 TIMEZONE = bimatquocgia.TIMEZONE
 AMAZON_ZIP_CODE = bimatquocgia.ZIP_CODE
 USER_AGENTS = [
@@ -43,7 +46,55 @@ except gspread.exceptions.WorksheetNotFound:
     log_sheet.append_row(["Timestamp", "URL", "Price"], value_input_option="RAW")
 
 # === SCRAPERS ===
+alternateAPI = -1
+badAPI = [True, True, True, True]
+errorAPI = [10, 10, 5, 5]
+
+def fatalError(status, no_retry, indexAPI):
+    global badAPI
+    global errorAPI
+    for i in no_retry:
+        if status == i:
+            print("! Fatal Error for", indexAPI, "with", status)
+            badAPI[indexAPI] = False
+            errorAPI[indexAPI] = 0
+            break
+
+def sumArr(arr):
+    sumA = 0
+    for i in arr:
+        sumA += i
+    return sumA
+
+def get_soup(target):
+    global alternateAPI
+    global badAPI
+    global errorAPI
+    run = True
+    while 1:
+        alternateAPI += 1
+        if sumArr(errorAPI) == 0:
+            if(badAPI[0]):
+                errorAPI[0] = 15
+            if(badAPI[1]):
+                errorAPI[1] = 15
+            if(badAPI[2]):
+                errorAPI[2] = 3
+            if(badAPI[3]):
+               errorAPI[3] = 4
+        if alternateAPI == 0 and errorAPI[0]:
+            return get_soup_scrapingant(target)
+        if alternateAPI == 1 and errorAPI[1]:
+            return get_soup_scrapingrobot(target)
+        if alternateAPI == 2 and errorAPI[2]:
+            return get_soup_hasdata(target)
+        if alternateAPI == 3:
+            alternateAPI = -1
+            if errorAPI[3]:
+                return get_soup_WebScrapingDotAi(target)
+
 def get_soup_scrapingant(target):
+    print("# Fetching Scraping Ant API")
     # Get html using ScrapingAnt
     conn = http.client.HTTPSConnection("api.scrapingant.com")
     conn.request("GET", "/v2/general?url=" + target + "&x-api-key=" + SCRAPINGANT_API_KEY + "&proxy_country=US&return_page_source=true")
@@ -52,9 +103,12 @@ def get_soup_scrapingant(target):
     data = res.read()
     html = data.decode("utf-8")
 
+    fatalError(res.status, no_retry = [400, 403, 500], indexAPI = 0)
+
     return BeautifulSoup(html, "html.parser") # Translate to soup
 
 def get_soup_hasdata(target):
+    print("# Fetching HasData")
     conn = http.client.HTTPSConnection("api.hasdata.com")
 
     payload = "{\"url\":\"" + target + "\",\"proxyType\":\"datacenter\",\"proxyCountry\":\"US\",\"blockResources\":true,\"blockAds\":false,\"blockUrls\":[],\"jsScenario\":[],\"screenshot\":false,\"jsRendering\":false,\"extractEmails\":false,\"includeOnlyTags\":[],\"excludeTags\":[],\"outputFormat\":[\"html\"]}"
@@ -70,9 +124,12 @@ def get_soup_hasdata(target):
     data = res.read()
     html = data.decode("utf-8")
 
+    fatalError(res.status, no_retry = [401, 403, 500], indexAPI = 2)
+
     return BeautifulSoup(html, "html.parser")
 
 def get_soup_WebScrapingDotAi(target):
+    print("# Fetching Web Scaping Dot Ai")
     params = {
         "api_key": WebScrapingDotAi_API_KEY,
         "url": target,
@@ -82,29 +139,38 @@ def get_soup_WebScrapingDotAi(target):
     response = requests.get('https://api.webscraping.ai/html', params=params)
     html = response.text
 
+    fatalError(response.status_code, no_retry = [400, 402, 403], indexAPI = 3)
+
     return BeautifulSoup(html, "html.parser")
 
 def get_soup_scrapingrobot(target):
+    print("# Fetching Scraping Robot")
     url = "https://api.scrapingrobot.com/?token=" + SCRAPINGROBOT_API_KEY + "&url=" + target
     response = requests.get(url)
-    html = response.json()['result']
+    data = response.json()
 
-    return BeautifulSoup(html, "html.parser")
+    if 'result' in data:
+        html = data['result']
+        return BeautifulSoup(html, "html.parser")
+    elif 'error' in data:
+        fatalError(
+            data['error'],
+            no_retry = [
+                "Token query parameter not found",
+                "Invalid client token",
+                "You do not have enough credits",
+                "Request-body is not a valid JSON",
+                "Internal server error"
+            ],
+            indexAPI = 1)
+
+    return BeautifulSoup("<html></html>", "html.parser")
 
 def get_wayfair(target):
-    soup = get_soup_scrapingrobot(target)
+    soup = get_soup(target)
     price_tag = soup.find(attrs={"data-name-id": "PriceDisplay"})
     if price_tag:
         return float(price_tag.get_text(strip=True)[1:])
-    return None
-
-def get_amazon_backup(target):
-    soup = get_soup_scrapingant(target)
-    price_tag = soup.find(attrs={"id": "corePriceDisplay_desktop_feature_div"})
-    if price_tag:
-        price_tag_splitted = price_tag.get_text(strip=True).split()
-        price_tag_splitted_second_time = price_tag_splitted[0][1:].split("$")
-        return float(price_tag_splitted_second_time[0].replace(",", ""))
     return None
 
 def get_amazon(target, location = AMAZON_ZIP_CODE):
@@ -148,8 +214,17 @@ def get_amazon(target, location = AMAZON_ZIP_CODE):
     except Exception:
         return get_amazon_backup(target)
 
+def get_amazon_backup(target):
+    soup = get_soup_scrapingant(target)
+    price_tag = soup.find(attrs={"id": "corePriceDisplay_desktop_feature_div"})
+    if price_tag:
+        price_tag_splitted = price_tag.get_text(strip=True).split()
+        price_tag_splitted_second_time = price_tag_splitted[0][1:].split("$")
+        return float(price_tag_splitted_second_time[0].replace(",", ""))
+    return None
+
 def fetch_price(target):
-    retries = 5
+    retries = 6
     while retries:
         retries -= 1
 
@@ -169,6 +244,8 @@ def fetch_price(target):
             return None
         if price:
             return price
+
+        badAPI[alternateAPI] -= 1
         # If failed, retry
         print("Extracting failed, retries remaining", retries)
         time.sleep(3)
@@ -189,6 +266,7 @@ if __name__ == "__main__":
         exit()
 
     results = []
+
     for url in urls:
         price = fetch_price(url)
         print(f"{url} -> {price}")
